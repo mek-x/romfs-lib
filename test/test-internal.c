021be3b10b4fbaa8dbafb8eeb89b08e3fce95e6e
Kamil Wcisło <mek.xgt@gmail.com>
2022-01-13 01:59:51 +0100

    test: add VolumeConfigure tests

--- test/test-internal.c
+++ test/test-internal.c
@@ -1,6 +1,7 @@
 #include "unity_fixture.h"
 
-#include "romfs.h"
+#include <errno.h>
+
 #include "romfs-internal.h"
 
 extern unsigned char empty_romfs[];
@@ -12,19 +13,35 @@ TEST_GROUP(internal);
 
 TEST_SETUP(internal)
 {
-    RomfsLoad(empty_romfs, empty_romfs_len);
 }
 
 TEST_TEAR_DOWN(internal)
 {
 }
 
-TEST(internal, SomeTest)
+TEST(internal, VolumeConfigureBadImg)
 {
-    TEST_ASSERT(0);
+    uint8_t buf[10] = { 0 };
+    volume_t vol;
+
+    int ret = RomfsVolumeConfigure(buf, &vol);
+    TEST_ASSERT_EQUAL_INT(-EINVAL, ret);
+}
+
+TEST(internal, VolumeConfigureGoodImg)
+{
+    volume_t vol;
+
+    int ret = RomfsVolumeConfigure(empty_romfs, &vol);
+    TEST_ASSERT_EQUAL_INT(0, ret);
+    TEST_ASSERT_EQUAL_STRING_LEN("empyt", vol.name, 5);
+    TEST_ASSERT_EQUAL_INT(96, vol.size);
+    TEST_ASSERT_EQUAL_HEX(0x20, vol.rootOff);
+    TEST_ASSERT_EQUAL_HEX(0xC7B9AC8D, vol.chksum);
 }
 
 TEST_GROUP_RUNNER(internal)
 {
-    RUN_TEST_CASE(internal, SomeTest);
+    RUN_TEST_CASE(internal, VolumeConfigureBadImg);
+    RUN_TEST_CASE(internal, VolumeConfigureGoodImg);
 }
